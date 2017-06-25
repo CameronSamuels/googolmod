6e91c14416aa0a0970f3456cb279a0721f33d880
CameronSamuels <satchmosanders@gmail.com>
2017-06-25 16:52:10 -0500

    Minified a.js

--- a.js
+++ a.js
@@ -1,7 +1,1 @@
-function a(e) { return document.querySelector(e) }
-function b() { 
-	chrome.storage.sync.set({'GoogolURL': a('input').value});
-	a('img').src = a('input').value
-}
-chrome.storage.sync.get("GoogolURL", function(e){a('input').value = e.GoogolURL;a('img').src = e.GoogolURL});
-a('div').onclick = b
+function a(o){return document.querySelector(o)}function b(){chrome.storage.sync.set({GoogolURL:a("input").value}),a("img").src=a("input").value}chrome.storage.sync.get("GoogolURL",function(o){a("input").value=o.GoogolURL,a("img").src=o.GoogolURL}),a("div").onclick=b
