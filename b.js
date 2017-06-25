a82fa2e1db10720fe028de5ea03b78033fbdc398
CameronSamuels <satchmosanders@gmail.com>
2017-06-25 17:01:58 -0500

    Fixed function id() and a()

--- b.js
+++ b.js
@@ -3,25 +3,25 @@ function a(d) { return document.getElementById(d) }
 function b() {
 	chrome.storage.sync.get("GoogolURL", function(c){
 		try {
-			id('logocont').innerHTML = "<img id='gl1' src='" + c.GoogolURL + "' />";
-			id('gl1').style.width = "85px";
-			id('gl1').style.height = "27.5px";
+			a('logocont').innerHTML = "<img id='gl1' src='" + c.GoogolURL + "' />";
+			a('gl1').style.width = "85px";
+			a('gl1').style.height = "27.5px";
 		} catch (ex) {}
 		try {
-			id('hplogo').src = c.GoogolURL;
-			id('hplogo').setAttribute('srcset', "");
-			id('hplogo').style.width = "340px";
-			id('hplogo').style.height = "110px";
+			a('hplogo').src = c.GoogolURL;
+			a('hplogo').setAttribute('srcset', "");
+			a('hplogo').style.width = "340px";
+			a('hplogo').style.height = "110px";
 		} catch (ex) {}
 		try {
 			document.querySelectorAll(".logo")[0].innerHTML = "<img id='gl2' src='" + c.GoogolURL + "' />";
-			id('gl2').style.width = "170px";
-			id('gl2').style.height = "55px";
+			a('gl2').style.width = "170px";
+			a('gl2').style.height = "55px";
 		} catch (ex) {}
 		try {
-			id("logo").innerHTML = "<img id='gl3' src='" + c.GoogolURL + "' />";
-			id('gl3').style.width = "198px";
-			id('gl3').style.height = "37px";
+			a("logo").innerHTML = "<img id='gl3' src='" + c.GoogolURL + "' />";
+			a('gl3').style.width = "198px";
+			a('gl3').style.height = "37px";
 		} catch (ex) {}
 		try {
 			document.querySelectorAll(".logo")[0].src = c.GoogolURL;
