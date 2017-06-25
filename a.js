d8e5585b02f0d9870e20b3e67be2826b831a0d5e
CameronSamuels <satchmosanders@gmail.com>
2017-06-25 16:05:51 -0500

    Revamped

--- a.js
+++ a.js
@@ -1,8 +1,7 @@
 function a(e) { return document.querySelector(e) }
 function b() { 
 	chrome.storage.sync.set({'GoogolURL': a('input').value});
-	a('img').src = a('input').value;
+	a('img').src = a('input').value
 }
-chrome.storage.sync.get("GoogolURL", function(obj){a('input').value = obj.GoogolURL;});
-chrome.storage.sync.get("GoogolURL", function(obj){a('img').src = obj.GoogolURL;});
-a('div').onclick = b()
+chrome.storage.sync.get("GoogolURL", function(e){a('input').value = e.GoogolURL;a('img').src = e.GoogolURL});
+a('div').onclick = b
