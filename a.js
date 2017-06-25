2de3cbe6306adb8e4a3f8e633420b858108c3be4
CameronSamuels <satchmosanders@gmail.com>
2017-06-25 16:36:14 -0500

    Quick save

--- a.js
+++ a.js
@@ -1,27 +1,27 @@
 function a(e) { return document.querySelector(e) }
 function b() { 
-	chrome.storage.sync.set({'GoogolURL': c(a('input').value)});
-	a('img').src = c(a('input').value)
+	chrome.storage.sync.set({'GoogolURL': a('input').value});
+	a('img').src = a('input').value
 }
-function c(url) {
-    var img = new Image();
+// function c(url) {
+//     var img = new Image();
 
-    img.setAttribute('crossOrigin', 'anonymous');
+//     img.setAttribute('crossOrigin', 'anonymous');
 
-    img.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width =this.width;
-        canvas.height =this.height;
+//     img.onload = function () {
+//         var canvas = document.createElement("canvas");
+//         canvas.width =this.width;
+//         canvas.height =this.height;
 
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(this, 0, 0);
+//         var ctx = canvas.getContext("2d");
+//         ctx.drawImage(this, 0, 0);
 
-        var dataURL = canvas.toDataURL("image/png");
+//         var dataURL = canvas.toDataURL("image/png");
 
-        alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
-    };
+//         alert(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+//     };
 
-    img.src = url;
-}
+//     img.src = url;
+// }
 chrome.storage.sync.get("GoogolURL", function(e){a('input').value = e.GoogolURL;a('img').src = e.GoogolURL});
 a('div').onclick = b
