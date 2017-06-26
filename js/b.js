aed7a2ab07c1d0e2eed6068c8b988a42e320e552
Cameron Samuels <satchmosanders@gmail.com>
2017-06-26 14:59:37 -0500

    Attempted to cahnge src on right click

--- js/b.js
+++ js/b.js
@@ -1,4 +1,9 @@
-var u = 0;
+var u = 0, el;
 chrome.storage.sync.get("GoogolURL", function(c){u = c.GoogolURL});
+function mycallback(info, tab) {
+    chrome.tabs.sendMessage(tab.id, "getClickedEl", function(clickedEl) {
+        el = clickedEl;
+    });
+}
 chrome.contextMenus.removeAll();
-var changeLogoContextMenu = chrome.contextMenus.create({title: "Change this logo", contexts:["image"], onclick: function(){alert(this.src);this.src=u;alert(this.src)}});
+var changeLogoContextMenu = chrome.contextMenus.create({title: "Change this logo", contexts:["image"], onclick: function(){alert(el.src);el.src=u;alert(el.src)}});
