6d97f50467edcb1dbc4111042395a2482e05efe0
Cameron Samuels <satchmosanders@gmail.com>
2017-06-26 14:55:28 -0500

    A lot - context menus
    Added context menus, added background script, renamed b.js to c.js. More

--- js/b.js
+++ js/b.js
@@ -1,35 +1,4 @@
-function a(d) { return document.getElementById(d) }
-function b() {
-	chrome.storage.sync.get("GoogolURL", function(c){
-		try {
-			a('logocont').innerHTML = "<img id='gl1' src='" + c.GoogolURL + "'>";
-			a('gl1').style.width = "85px";
-			a('gl1').style.height = "27.5px";
-		} catch (ex) {}
-		try {
-			a('hplogo').src = c.GoogolURL;
-			a('hplogo').removeAttribute('srcset');
-			a('hplogo').style.width = "340px";
-			a('hplogo').style.height = "110px";
-		} catch (ex) {}
-		try {
-			document.querySelectorAll(".logo")[0].innerHTML = "<img id='gl2' src='" + c.GoogolURL + "'>";
-			a('gl2').style.width = "170px";
-			a('gl2').style.height = "55px";
-		} catch (ex) {}
-		try {
-			a("logo").innerHTML = "<img id='gl3' src='" + c.GoogolURL + "'>";
-			a('gl3').style.width = "114px";
-			a('gl3').style.height = "37px";
-		} catch (ex) {}
-		try {
-			document.querySelectorAll(".product-logo")[0].innerHTML = "<img id='gl4' src='" + c.GoogolURL + "'>";
-			a('gl4').style.maxWidth = '105px';
-			a('gl4').style.maxHeight = '34px';
-		} catch (ex) {}
-		try {
-			document.querySelectorAll(".logo")[0].src = c.GoogolURL;
-		} catch (ex) {}
-	});
-}
-b();
+var u = 0;
+chrome.storage.sync.get("GoogolURL", function(c){u = c.GoogolURL});
+chrome.contextMenus.removeAll();
+var changeLogoContextMenu = chrome.contextMenus.create({title: "Change this logo", contexts:["image"], onclick: function(){alert(this.src);this.src=u;alert(this.src)}});
---
+++ js/c.js
@@ -0,0 +1,36 @@
+function a(d) { return document.getElementById(d) }
+function b() {
+	chrome.storage.sync.get("GoogolURL", function(c){
+		u = c.GoogolURL;
+	try {
+		a('logocont').innerHTML = "<img id='gl1' src='" + u + "'>";
+		a('gl1').style.width = "85px";
+		a('gl1').style.height = "27.5px";
+	} catch (ex) {}
+	try {
+		a('hplogo').src = u;
+		a('hplogo').removeAttribute('srcset');
+		a('hplogo').style.width = "340px";
+		a('hplogo').style.height = "110px";
+	} catch (ex) {}
+	try {
+		document.querySelectorAll(".logo")[0].innerHTML = "<img id='gl2' src='" + u + "'>";
+		a('gl2').style.width = "170px";
+		a('gl2').style.height = "55px";
+	} catch (ex) {}
+	try {
+		a("logo").innerHTML = "<img id='gl3' src='" + u + "'>";
+		a('gl3').style.width = "114px";
+		a('gl3').style.height = "37px";
+	} catch (ex) {}
+	try {
+		document.querySelectorAll(".product-logo")[0].innerHTML = "<img id='gl4' src='" + u + "'>";
+		a('gl4').style.maxWidth = '105px';
+		a('gl4').style.maxHeight = '34px';
+	} catch (ex) {}
+	try {
+		document.querySelectorAll(".logo")[0].src = u;
+	} catch (ex) {}
+	});
+}
+b();
